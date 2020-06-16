acc1e2072388dc42983967dbb5737516e567e713
Jamester-Cloud <52103403+Jamester-Cloud@users.noreply.github.com>
2020-06-16 16:30:08 -0400

    Add files via upload

--- src/keys.js
+++ src/keys.js
@@ -1,9 +1,10 @@
 module.exports={
     database: {
-        host: "localhost",
-        user: 'root',
-        password: '',
-        database: 'enterprise',
-        acquireTimeout: 30000
+        host: "ec2-52-202-146-43.compute-1.amazonaws.com",
+	    user: 'qimlnyuyjrtgpj',
+	    password: 'bf6d928a02aef6c0a2c377a5f5cbd5ff57ec9c2b1d9f3d6c7bf135186e3e0c95',
+	    database: 'd4af7nba4huah8',
+	    acquireTimeout: 30000,
+	    multipleStatements: true
     }
 }
