57953e19089fef493d2338cd185699bc5495dd0b
Jamester-Cloud <52103403+Jamester-Cloud@users.noreply.github.com>
2020-06-16 17:03:12 -0400

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -1,9 +1,9 @@
 module.exports={
     database: {
-        host: "ec2-52-202-146-43.compute-1.amazonaws.com",
-	    user: 'qimlnyuyjrtgpj',
-	    password: 'bf6d928a02aef6c0a2c377a5f5cbd5ff57ec9c2b1d9f3d6c7bf135186e3e0c95',
-	    database: 'd4af7nba4huah8',
+            host: "byq1gbreuskh8wkndcb6-mysql.services.clever-cloud.com",
+	    user: 'uemirsa0qnhzyovr',
+	    password: 'quxqb21sLydg9TfHh3P3',
+	    database: 'byq1gbreuskh8wkndcb6',
 	    acquireTimeout: 30000,
 	    multipleStatements: true
     }
